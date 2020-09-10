6ea0705396b6d274d3cea9ba9a913d6dd2f2893a
Ryan Cocjin <revcocjin@yahoo.com>
2020-09-10 21:52:44 +0800

    Created product filter

---
+++ PogiShop/PogiShop.Core/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using PogiShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PogiShop.Core.ViewModels
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
