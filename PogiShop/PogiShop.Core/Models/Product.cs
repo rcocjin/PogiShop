945f5a5adb2462b64f80626a7163140aa466f61e
Ryan Cocjin <revcocjin@yahoo.com>
2020-09-21 21:12:00 +0800

    Finished Basket Service

--- PogiShop/PogiShop.Core/Models/Product.cs
+++ PogiShop/PogiShop.Core/Models/Product.cs
@@ -15,7 +15,7 @@ namespace PogiShop.Core.Models
         public string Name { get; set; }
         public string Description { get; set; }
         [Range (0,1000)]
-        public string Price { get; set; }
+        public decimal Price { get; set; }
         public string Category { get; set; }
         public string Image { get; set; }
 
